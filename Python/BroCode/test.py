b0133f28f53941d932f8fb3b92ace7e69b4f4c1c
Coop04 <aswanthsatheesh432@gmail.com>
2025-05-12 21:26:01 +0530

    .

--- Python/BroCode/test.py
+++ Python/BroCode/test.py
@@ -179,3 +179,396 @@
 
 # import sys
 # print(sys.argv)
+# -------------------------------
+# import cmath
+# a= int(input("Enter a"))
+# if(a==0):
+#     print("This is not a qe ")
+# b= int(input("Enter b"))
+# c= int(input('Enter c'))
+# des = cmath.sqrt(b**2-(4*a*c))
+# r1 = (-b + des) / (2*a)
+# r2 = (-b - des) / (2*a)
+# print(f'the roots are {r1} and {r2}')
+
+# ------------------------------------
+
+# a = input('Enter a no.')
+# l = len(a)
+# sum = 0
+# for i in a:
+#     sum+=int(i)**l
+# if(sum==int(a)):
+#     print('Amstrong')
+# else:
+#     print("Not")
+# ----------------------------------
+
+# u = int(input("upper limit: "))
+# l = int(input('lower limit: '))
+# for i in range(l,u+1):
+#     if(i%2!=0):
+#         print(i)
+
+# ---------------------------------
+
+# def fact(n):
+#     if(n==0):
+#         return 1
+#     else:
+#         return(n*fact(n-1))
+    
+# limit = int(input('Enter the limit'))
+# x = int(input('Enter the value of x'))
+
+# sum=0
+# for i in range(0,limit):
+#     sum+=(x**i)/fact(i)
+# print(f'Sum of {limit} terms = {sum}')
+
+# -----------------------------------------
+
+# with open('file1.txt','r') as f:
+#     count=0
+#     for i in f:
+#         line = i.strip()
+#         words = line.split(' ')
+#         for j in words:
+#             if(len(j)==4):
+#                 count+=1
+#             else:
+#                 continue
+# print(count)
+
+# ----------------------------------
+
+# import math
+# nums = []
+# n = int(input('Enter no.of nums'))
+# print('The numbers are: ')
+# for i in range(n):
+#     k = int(input())
+#     nums.append(k)
+
+# def mean(nums):
+#     sum = 0
+#     for i in nums:
+#         sum+=i
+#     return sum/len(nums)
+
+# def median(nums):
+#     nums.sort()
+#     if(len(nums)%2==0):
+#         return (nums[len(nums)//2]+nums[(len(nums)//2)-1])/2
+#     else:
+#         return nums[(len(nums)//2)]
+    
+# def mode(nums):
+#     a = list(set(nums))
+#     d = {}
+#     for i in a:
+#         d[i] = nums.count(i)
+#     return max(d.values())
+
+# print(mean(nums), median(nums), mode(nums))
+
+# ------------------------------------------------
+
+# def sub_check(l, s):
+#     k = 0  # Pointer for list s
+#     for i in range(len(l)):
+#         if k < len(s):
+#             if l[i] == s[k]:
+#                 k += 1  # Move the pointer in s if there's a match
+#             if k == len(s):  # If all elements in s are matched
+#                 return True
+#             if (k>0 and l[i]!=s[k]):
+#                     break
+        
+#     if k == len(s):  # If all elements in s are matched
+#         return True
+#     return False  # If not all elements in s are matched
+
+# # Test cases
+# l = [1, 2, 3, 4]
+# s1 = [2, 4]  # Not a subsequence
+# s2 = [3, 4]  # Is a subsequence
+# print(sub_check(l, s1))  # Output: False
+# print(sub_check(l, s2))  # Output: True
+
+# ----------------------------------------------
+
+# import turtle
+
+# t = turtle.Turtle()
+# t.color('black','red')
+# t.begin_fill()
+# for i in range(5):
+#     t.forward(100)
+#     t.left(144)
+# t.end_fill()
+
+# turtle.done()
+
+# -----------------------------
+
+# class Rectangle:
+#     def __init__(self,length,breadth):
+#         self.length = length
+#         self.breadth = breadth
+#     def area(self):
+#         return self.length*self.breadth
+#     def peri(self):
+#         return 2*(self.length+self.breadth)
+    
+# r = Rectangle(10,20)
+# print(r.area(),r.peri())
+
+# -----------------------------------------
+
+# class Student:
+#     def __init__(self,name,roll):
+#         self.name=name
+#         self.roll=roll
+#         self.age=None
+#         self.marks=None
+    
+#     def setAge(self,age):
+#         self.age = age
+
+#     def setMarks(self,marks):
+#         self.marks = marks
+    
+#     def display(self):
+#         if self.age is not None and self.marks is not None:
+#             print(f"Name: {self.name}, Roll: {self.roll}, Age: {self.age}, Marks: {self.marks}")
+#         else:
+#             print("Age or Marks not set.")
+    
+# s = Student('Anu',11)
+# # s.setAge(19)
+# # s.setMarks(33)
+# print(s.display())
+
+# ----------------------------------------------------
+
+# import numpy as np
+# a= np.array([[1,2,3],[4,5,6],[7,8,9]])
+# print(a)
+
+# print(a[:2])
+# print(a[:2,:1])
+# print(a[1,:2]) 
+# a[:2,1:]=0
+# print(a) 
+
+# ---------------------------------------
+
+# toppers = {'regNo.':['a','b','c'],
+#            'name':['anu','binu','jinu'],
+#            'sem':[3,4,5],
+#            'cgpa':[9,9.8,9.9]}
+
+# import pandas as pd
+
+# df = pd.DataFrame(toppers)
+# print(df)
+
+# print(df.shape)
+# print()
+# print(df.head())
+
+# --------------------------------
+
+# import pandas as pd
+# from pylab import *
+
+# df = pd.read_csv('toppers.csv',index_col='regNo.')
+# print(df)
+
+# plot(df['sem'],df['cgpa'],'r.--',label='sem x cgpa')
+# title('Toppers')
+# xlabel('sem')
+# ylabel('cgpa')
+# legend()
+# grid()
+# show()
+
+# ------------------------------------------------
+
+# def fact(n):
+#     if(n==0):
+#         return 1
+#     else:
+#         return(n*fact(n-1))
+    
+# limit = int(input('Enter the limit'))
+# x = int(input('Enter the value of x'))
+
+# sum=0
+# for i in range(0,limit):
+#     if(i%2==0):
+#         sum+=(x**i)/fact(i)
+#     else:
+#         sum-=(x**i)/fact(i)
+# print(f'Sum of {limit} terms = {sum}')
+# ---------------------------------------------
+
+# def power(x,y):
+#     p=1
+#     for i in range(0,y):
+#         p *= x 
+#     return p
+
+# print(power(3,3))
+
+# -----------------------------------
+
+# l = [-1,-3,4,5,-6,2,0,-4]
+
+# pos = lambda x: True if x>0 else False
+# # print(pos(1))
+# l1 = list(filter(pos,l))
+# print(l)
+# print(l1)
+
+# --------------------------------
+
+# import turtle 
+
+# t = turtle.Turtle()
+# t.color('black','yellow')
+# t.begin_fill()
+# t.circle(100)
+# t.end_fill()
+
+# turtle.done()
+
+# ---------------------------------
+
+# class Complex:
+#     def __init__(self,real,imag):
+#         self.real = real
+#         self.imag = imag
+    
+#     def __add__(self,other):
+#         return (self.real+other.real,self.imag+other.imag)
+    
+#     def __mul__(self,other):
+#          return (self.real*other.real,self.imag*other.imag)
+    
+# c = Complex(1,2)
+# p = Complex(3,4)
+# print(c+p)
+# print(c*p)
+
+# -------------------------------------------
+
+# from math import gcd
+
+# class Rational:
+#     def __init__(self, numerator, denominator):
+#         if denominator == 0:
+#             raise ValueError("Denominator cannot be zero.")
+#         self.numerator = numerator
+#         self.denominator = denominator
+#         self.reduce()  # Automatically reduce the fraction upon initialization
+
+#     def reduce(self):
+#         """Simplify the fraction."""
+#         common_divisor = gcd(self.numerator, self.denominator)
+#         self.numerator //= common_divisor
+#         self.denominator //= common_divisor
+#         return self
+
+#     def __add__(self, other):
+#         """Add two rational numbers."""
+#         if not isinstance(other, Rational):
+#             raise TypeError("Can only add Rational numbers.")
+#         new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+#         new_denominator = self.denominator * other.denominator
+#         return Rational(new_numerator, new_denominator).reduce()
+
+#     def __lt__(self, other):
+#         """Compare two rational numbers (less than)."""
+#         if not isinstance(other, Rational):
+#             raise TypeError("Can only compare Rational numbers.")
+#         return self.numerator * other.denominator < other.numerator * self.denominator
+
+#     def __str__(self):
+#         """String representation of the rational number."""
+#         return f"{self.numerator}/{self.denominator}"
+
+
+# # Example Usage
+# r1 = Rational(3, 4)
+# r2 = Rational(2, 6)
+
+# print("Rational 1:", r1)
+# print("Rational 2:", r2)
+
+# # Simplify fractions
+# print("Simplified Rational 1:", r1.reduce())
+# print("Simplified Rational 2:", r2.reduce())
+
+# # Add two rational numbers
+# r3 = r1 + r2
+# print("Sum of Rational 1 and Rational 2:", r3)
+
+# # Compare two rational numbers
+# print("Is Rational 1 less than Rational 2?", r1 < r2)
+
+# =================================================
+
+# try:
+#     with open('file2.tx','w') as f:
+#         f.write('Hello Good Morning')
+# except FileNotFoundError as e:
+#     print(e)
+# except PermissionError as e:
+#     print(e)
+# except IOError as e:
+#     print(e)
+# finally:
+#     print('File ops are completed')
+
+# ---------------------------------------------
+
+# import os 
+# for items in os.listdir('.'):
+#     print(items)
+
+# -------------------------------------------
+
+# import numpy as np
+# a = np.random.randint(1,20,size=(3,3))
+# print(a)
+# b = np.random.randint(1,20,size=(3,3))
+# print(b)
+
+# print('sum:')
+# print(a+b)
+# print('product:')
+# m = a@b
+# print(m)
+# print('transpose of product:')
+# print(m.T)
+# print('trace')
+# print(np.trace(m))
+
+# --------------------------------------
+
+# def contains_sublist(main_list, sub_list):
+#     n = len(sub_list)
+#     for i in range(len(main_list) - n + 1):
+#         if main_list[i:i+n] == sub_list:
+#             return True
+#     return False
+
+# l = [1,2,3,4,5,6]
+# s = [2,3]
+# y = [2,4]
+# print(contains_sublist(l,s))
+# print(contains_sublist(l,y))
+
+# ----------------------------------------
